5ae2466fa5118dd005a63e6e0b9f57180b11ac43
akkaiba <74521113+akkaiba@users.noreply.github.com>
2021-02-18 10:46:51 +0100

    Add files via upload

---
+++ pyTool/tools/addons/logo.py
@@ -0,0 +1,17 @@
+# Import modules
+from colorama import Fore
+
+# Logo
+print(rf"""{Fore.CYAN}
+   dBBBBBb    dBBBBP dBBBBP dBP dBP dBP dBBBP.dBBBBP
+       dBP   dBP.BP dBP.BP d8P.dBP           BP     
+   dBBBBK'  dBP.BP dBP.BP dBBBBP  dBP dBBP   `BBBBb 
+  dBP  BB  dBP.BP dBP.BP dBP BB  dBP dBP        dBP 
+ dBP  dB' dBBBBP dBBBBP dBP dBP dBP dBBBBP dBBBBP'  
+                                                    
+  dBBBBBBP dBBBBP dBBBBP dBP    dBP dBP dBP dBBBBBBP
+          dBP.BP dBP.BP        d8P.dBP              
+   dBP   dBP.BP dBP.BP dBP    dBBBBP  dBP    dBP    
+  dBP   dBP.BP dBP.BP dBP    dBP BB  dBP    dBP     
+ dBP   dBBBBP dBBBBP dBBBBP dBP dBP dBP    dBP      
+{Fore.RESET}""")
